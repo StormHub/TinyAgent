b7d92a26feeb44a85a4c04054b0370408230a181
johnny <johnny.zou@azenix.com.au>
2024-05-29 15:03:36 +1000

    Format.

--- src/TinyAgents.Search/GeographyPointOperations.cs
+++ src/TinyAgents.Search/GeographyPointOperations.cs
@@ -9,23 +9,22 @@ internal sealed class GeographyPointOperations : SpatialOperations
     public override double Distance(Geography operand1, Geography operand2)
     {
         if (operand1 is not GeographyPoint point1)
-        {
             throw new ArgumentException($"{nameof(GeographyPoint)} expected", nameof(operand1));
-        }
         if (operand2 is not GeographyPoint point2)
-        {
             throw new ArgumentException($"{nameof(GeographyPoint)} expected", nameof(operand2));
-        }
 
         var dLat = ToRadians(point2.Latitude - point1.Latitude);
         var dLon = ToRadians(point2.Longitude - point1.Longitude);
 
         var a = Math.Pow(Math.Sin(dLat / 2), 2)
-                   + Math.Pow(Math.Cos(ToRadians(point1.Latitude)), 2) * Math.Pow(Math.Sin(dLon / 2), 2);
+                + Math.Pow(Math.Cos(ToRadians(point1.Latitude)), 2) * Math.Pow(Math.Sin(dLon / 2), 2);
         var centralAngle = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
-        
+
         return EarthRadius * centralAngle;
     }
 
-    private static double ToRadians(double angle) => angle * (Math.PI / 180);
+    private static double ToRadians(double angle)
+    {
+        return angle * (Math.PI / 180);
+    }
 }
--- src/TinyAgents.Search/SearchPlugin.cs
+++ src/TinyAgents.Search/SearchPlugin.cs
@@ -9,9 +9,14 @@ namespace TinyAgents.Search;
 
 internal sealed class SearchPlugin
 {
-    private readonly SearchClient _searchClient;
     private readonly ILogger _logger;
-    
+    private readonly SearchClient _searchClient;
+
+    static SearchPlugin()
+    {
+        SpatialImplementation.CurrentImplementation.Operations = new GeographyPointOperations();
+    }
+
     public SearchPlugin(SearchClient searchClient, ILogger<SearchPlugin> logger)
     {
         _searchClient = searchClient;
@@ -31,20 +36,20 @@ internal sealed class SearchPlugin
         };
         var fieldName = nameof(LocationIndex.Point).ToLowerInvariant();
         var point = GeographyPoint.Create(latitude, longitude);
-        
+
         searchOptions.Filter = GeographyDistanceInRange(
             point, 100, fieldName);
         searchOptions.OrderBy.Add(DistanceAscending(point, fieldName));
 
         var response = await _searchClient.SearchAsync<LocationIndex>("*", searchOptions);
-        _logger.LogInformation("Location ({latitude}, {longitude}) {TotalCount}", 
+        _logger.LogInformation("Location ({latitude}, {longitude}) {TotalCount}",
             latitude, longitude, response.Value.TotalCount);
 
         var buffer = new StringBuilder();
         await foreach (var result in response.Value.GetResultsAsync())
         {
             buffer.Append(result.Document.GetText());
-            
+
             var distance = result.Document.Point.Distance(point);
             buffer.AppendLine($" kilometers: {distance}");
         }
@@ -52,19 +57,23 @@ internal sealed class SearchPlugin
         return buffer.ToString();
     }
 
-    private static string DistanceAscending(GeographyPoint point, string fieldName) =>
-        $"{GeographyDistanceTo(point, fieldName)} asc";
-    
-    private static string GeographyDistanceInRange(GeographyPoint point, int kilometers, string fieldName) =>
-        $"{GeographyDistanceTo(point, fieldName)} le {kilometers}";
-  
-    private static string GeographyDistanceTo(GeographyPoint point, string fieldName) =>
-        $"geo.distance({fieldName}, {GeographyPointOf(point)})";
-    private static string GeographyPointOf(GeographyPoint point) => 
-        $"geography'POINT({point.Longitude} {point.Latitude})'";
-    
-    static SearchPlugin()
+    private static string DistanceAscending(GeographyPoint point, string fieldName)
     {
-        SpatialImplementation.CurrentImplementation.Operations = new GeographyPointOperations();
+        return $"{GeographyDistanceTo(point, fieldName)} asc";
+    }
+
+    private static string GeographyDistanceInRange(GeographyPoint point, int kilometers, string fieldName)
+    {
+        return $"{GeographyDistanceTo(point, fieldName)} le {kilometers}";
+    }
+
+    private static string GeographyDistanceTo(GeographyPoint point, string fieldName)
+    {
+        return $"geo.distance({fieldName}, {GeographyPointOf(point)})";
+    }
+
+    private static string GeographyPointOf(GeographyPoint point)
+    {
+        return $"geography'POINT({point.Longitude} {point.Latitude})'";
     }
 }
