322315d9b8b54cb96870da5fc7a6179196ee63b1
johnny <johnny.zou@azenix.com.au>
2024-06-03 08:48:59 +1000

    Move to api folder.

---
+++ src/api/TinyAgent.Local/Hosting/AgentOptions.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TinyAgent.Local.Hosting;
+
+public sealed class AgentOptions
+{
+    [Required] public required Uri Uri { get; init; }
+
+    [Required(AllowEmptyStrings = false)] public required string ChannelName { get; init; }
+}
---
+++ src/api/TinyAgent.Local/Hosting/ConsoleHostBuilder.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace TinyAgent.Local.Hosting;
+
+internal static class ConsoleHostBuilder
+{
+    public static IHost Build(string[] args)
+    {
+        return Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((builderContext, builder) =>
+            {
+                builder.AddJsonFile("appsettings.json", false);
+                builder.AddJsonFile($"appsettings.{builderContext.HostingEnvironment.EnvironmentName}.json", true);
+
+                if (builderContext.HostingEnvironment.IsDevelopment()) builder.AddUserSecrets<Program>();
+
+                builder.AddEnvironmentVariables();
+            })
+            .ConfigureServices((builderContext, services) => { services.AddApplication(); })
+            .UseLogging()
+            .UseConsoleLifetime()
+            .Build();
+    }
+
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddOptions<AgentOptions>()
+            .BindConfiguration(nameof(AgentOptions))
+            .ValidateDataAnnotations();
+
+        services.AddTransient<AgentConnection>();
+
+        return services;
+    }
+
+    private static IHostBuilder UseLogging(this IHostBuilder builder)
+    {
+        // Fallback before configuration can be loaded
+        var logger = new LoggerConfiguration()
+            .WriteTo.Console(
+                outputTemplate:
+                "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} {CorrelationId} {Level:u3}] {Username} {Message:lj}{NewLine}{Exception}")
+            .CreateBootstrapLogger();
+
+        builder.UseSerilog((hostContext, provider, configuration) =>
+        {
+            if (hostContext.Configuration.GetSection(nameof(Serilog)).Exists())
+                configuration.ReadFrom.Configuration(hostContext.Configuration);
+        });
+
+        return builder;
+    }
+}
