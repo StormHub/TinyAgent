cc07008fa83907c489e441cac1ce405efec1e18f
johnny <johnny.zou@azenix.com.au>
2024-06-11 11:20:21 +1000

    Add restart to remove history.

--- src/api/TinyAgents.HubHost/Hosting/AgentHub.cs
+++ src/api/TinyAgents.HubHost/Hosting/AgentHub.cs
@@ -18,8 +18,7 @@ internal sealed class AgentHub(IAssistantAgentBuilder builder, ILogger<AgentHub>
         var id = Context.ConnectionId;
         if (!Agents.TryGetValue(id, out var agent))
         {
-            // agent = await builder.Build(AssistantAgentType.RouteDirections, Context.ConnectionAborted);
-            agent = await builder.Build(AssistantAgentType.ChargingLocations, Context.ConnectionAborted);
+            agent = await Build();
             Agents.AddOrUpdate(id, _ => agent, (_, _) => agent);
 
             _logger.LogInformation("Connected {Id}", id);
@@ -27,6 +26,7 @@ internal sealed class AgentHub(IAssistantAgentBuilder builder, ILogger<AgentHub>
 
         await base.OnConnectedAsync();
     }
+    
 
     public override async Task OnDisconnectedAsync(Exception? exception)
     {
@@ -35,6 +35,26 @@ internal sealed class AgentHub(IAssistantAgentBuilder builder, ILogger<AgentHub>
         _logger.LogInformation("Disconnected {Id}", id);
 
         await base.OnDisconnectedAsync(exception);
+    }
+
+    public async Task Restart()
+    {
+        var id = Context.ConnectionId;
+        if (Agents.TryRemove(id, out var agent))
+        {
+            await agent.DisposeAsync();
+            
+            // Recreate so that no more history
+            agent = await Build();
+            Agents.AddOrUpdate(id, _ => agent, (_, _) => agent);
+        }
+    }
+
+    private async Task<IAssistantAgent> Build()
+    {
+        // agent = await builder.Build(AssistantAgentType.RouteDirections, Context.ConnectionAborted);
+        var agent = await builder.Build(AssistantAgentType.ChargingLocations, Context.ConnectionAborted);
+        return agent;
     }
 
     public async IAsyncEnumerable<MessageContent> Streaming(
